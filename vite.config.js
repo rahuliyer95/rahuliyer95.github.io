97199bbec2165ce082c269be16b2e03540404df0
Rahul Iyer <rahuliyer573@gmail.com>
2024-03-02 19:03:17 -0800

    Update profile picture

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import * as path from "path";
 
 /** @type {import('vite').UserConfig} */
 export default {
+  publicDir: path.join(__dirname, "src", "static"),
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules", "bootstrap"),
